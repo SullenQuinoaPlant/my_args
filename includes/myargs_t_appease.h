9fe3a81a7b8b80e68b9bc2bde3ce0947495cd35e
SullenQuinoaPlant <noe.42.2017@gmail.com>
2018-09-07 12:27:40 +0200

    i

---
+++ includes/myargs_t_appease.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   myargs_t_appease.h                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmauvari <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/09/07 12:07:35 by nmauvari          #+#    #+#             */
+/*   Updated: 2018/09/07 12:10:03 by nmauvari         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MYARGS_T_APPEASE_H
+# define MYARGS_T_APPEASE_H
+
+/*
+**typedef struct	s_args_side_effects {
+**	statically	typed;
+**	stuff		here;
+**}					t_args_public_pointer_effective_arg_side_effects;
+*/
+
+typedef void*	t_args_public_pointer_effective_arg_side_effects;
+
+typedef t_args_public_pointer_effective_arg_side_effects	t_appease;
+
+#endif

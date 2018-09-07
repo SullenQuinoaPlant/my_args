6e18a025c9e80c6912263c9e1ae0c8791980f8d8
SullenQuinoaPlant <noe.42.2017@gmail.com>
2018-09-07 12:10:13 +0200

    i

---
+++ includes/myargs.h
@@ -0,0 +1,22 @@
+#ifndef MYARGS_H
+# define MYARGS_H
+
+/*
+** modify :
+** t_args_public_pointer_effective_arg_side_effects t_appease
+** in myargs_t_appease.h if wanted.
+** defaults to (void*)
+*/
+# include "myargs_struct_arg_state.h"
+typedef struct s_arg_arg_state			t_s_aas;
+
+typedef struct		s
+typedef struct		s_register_argument_type {
+	char	*name;
+
+
+}					t_s_rat;
+
+
+
+#endif

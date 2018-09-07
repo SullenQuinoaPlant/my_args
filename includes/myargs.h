cbc94e569488c2d6200acefe1271ede933571f7f
SullenQuinoaPlant <noe.42.2017@gmail.com>
2018-09-07 12:26:22 +0200

    i

--- includes/myargs.h
+++ includes/myargs.h
@@ -7,15 +7,35 @@
 ** in myargs_t_appease.h if wanted.
 ** defaults to (void*)
 */
-# include "myargs_struct_arg_state.h"
-typedef struct s_arg_arg_state			t_s_aas;
+# include "myargs_t_appease.h"
 
-typedef struct		s
-typedef struct		s_register_argument_type {
-	char	*name;
+/*
+**t_s_aaps:
+** args: all args array, as passed to main
+** argsc: all args count, aspassed to main
+** argsi: index of current argument
+** argi: index of first unused char within string of current argument
+*/
+typedef struct		s_args_arg_parse_state {
+	char		*args[];
+	int			argsc;
+	int			argsi;
+	char		*argi;
+}					t_s_aaps;
 
+/*
+**t_a_arger:
+** return < 0 on system errors,
+** 0 on success,
+** > 0 on our errors
+*/
+typedef int			(*t_a_arger)(t_s_aaps *parse_state, t_appease *arg_effects);
 
-}					t_s_rat;
+typedef struct		s_args_register_argument_type {
+	char		*name;
+	t_aarger	affect;
+
+}					t_s_a_rat;
 
 
 
